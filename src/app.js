06817c65145527ee01cdfb91b5ad92d843de7e45
anahiyapa <141771035+anahiyapa@users.noreply.github.com>
2023-08-15 21:04:23 +0100

    Humidity API working

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#cityinput");
   let temperatureElement = document.querySelector("#temperature-city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "caa3ff8c1a9cb6c9d488a903d52ad2dc";
