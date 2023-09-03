aa4666bc6a1984e129329571f6c3f4911ac84e50
anahiyapa <141771035+anahiyapa@users.noreply.github.com>
2023-09-03 11:52:09 +0100

    Wind Element API working

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let cityElement = document.querySelector("#cityinput");
   let temperatureElement = document.querySelector("#temperature-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = "Friday 500";
 }
 
 let apiKey = "caa3ff8c1a9cb6c9d488a903d52ad2dc";
